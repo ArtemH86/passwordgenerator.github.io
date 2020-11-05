9af8e2217a9273ee16a763ee41e8446034dd9c81
Artem <artyom_hovsepyan@yahoo.com>
2020-11-04 23:58:36 -0800

    modified scripts, new comments and redisgned

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # passwordgenerator.github.io
 password_generator_homework
+
+created by Artem Hovsepyan
--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // Assignment Code
 
+//adding variables that we will use
+var value = document.querySelector(".slider");
+var slide = document.querySelector(".slider").value;
+var bar_lentgh = document.querySelector("#value");
+bar_lentgh.textContent = "lentgh " + slide;
+
+value.addEventListener("change", function() {
+    slide = document.querySelector(".slider").value;
+    bar_lentgh.textContent = "lentgh, " + slide;
+})
+
 
 function generate() {
-    //var generateBtn = document.querySelector("#generate");
-    // list of possible values
+
+
+    //create array of all possible values
     var password_array = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    var range = document.getElementById("Range");
+    //adding empty array 
     var password = "";
 
-    //set minimum steps (temporary) this will be changed from outside;
-    var k = 6;
-
-    // This loop is for taking  random element from password_array and attaching together  
-    for (var i = 0; i < k; i++) {
+    // This loop is for taking  random elements from password_array and attaching together  
+    for (var i = 0; i < slide; i++) {
         password = password + password_array.charAt(Math.floor(Math.random() * Math.floor(password_array.length - 1)));
     }
     //sending password to textarea in html
     document.getElementById("password").value = password;
+
+    // just for show in console 
+    console.log(password);
 }
